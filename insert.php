6f14b243ea862330d9cc5458faa59f9a6f0c337e
ali <alihaiderriesn@gmail.com>
2023-05-05 22:01:10 +0300

    new pro

---
+++ insert.php
@@ -0,0 +1,27 @@
+
+<?php
+require('./config.php');
+
+
+// Validate form input
+if (isset($_POST["submit"])) {
+	$name = mysqli_real_escape_string($conn, $_POST["email"]);
+	$email = mysqli_real_escape_string($conn, $_POST["pass"]);
+	
+
+    // Check for empty fields
+    if (empty($name) || empty($email) ) {
+        echo "Please fill in all fields.";
+    } else {
+        // Insert form data into database
+        $sql = "INSERT INTO sign_in ( Email,Password) VALUES ('$name', '$email')";
+        if (mysqli_query($conn, $sql)) {
+            echo "Form submitted successfully.";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    }
+    
+    mysqli_close($conn);
+}
+?>    
